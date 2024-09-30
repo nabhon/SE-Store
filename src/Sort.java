67382c8100db5a12d5793ca3ae4752849920c274
NetCaffe <66160351@go.buu.ac.th>
2024-09-30 16:32:47 +0700

    9/30/24 Make sort Shorter

--- src/Sort.java
+++ src/Sort.java
@@ -3,73 +3,57 @@ import java.util.ArrayList;
 public class Sort {
 
     public static void sortByNameASC(ArrayList<Product> productShow){
-        Product temp,min;
-        int minIndex;
         for (int i = 0; i < productShow.size(); i++) {
-            temp = productShow.get(i);
-            min = productShow.get(i);
-            minIndex = i;
+            Product temp = productShow.get(i);
+            int minIndex = i;
             for (int j = i+1 ; j < productShow.size()  ; j++) {
-                if (min.findValue()>productShow.get(j).findValue()){
-                    min = productShow.get(j);
+                if (productShow.get(minIndex).findValue()>productShow.get(j).findValue()){
                     minIndex = j;
                 }
             }
-            productShow.set(i,min);
+            productShow.set(i,productShow.get(minIndex));
             productShow.set(minIndex,temp);
         }
     }
 
     public static void sortByNameDESC(ArrayList<Product> productShow){
-        Product temp,max;
-        int maxIndex;
         for (int i = 0; i < productShow.size(); i++) {
-            temp = productShow.get(i);
-            max = productShow.get(i);
-            maxIndex = i;
+            Product temp = productShow.get(i);
+            int maxIndex = i;
             for (int j = i+1 ; j < productShow.size()  ; j++) {
-                if (max.findValue()<productShow.get(j).findValue()){
-                    max = productShow.get(j);
+                if (productShow.get(maxIndex).findValue()<productShow.get(j).findValue()){
                     maxIndex = j;
                 }
             }
-            productShow.set(i,max);
+            productShow.set(i,productShow.get(maxIndex));
             productShow.set(maxIndex,temp);
         }
     }
 
     public static void sortByPriceASC(ArrayList<Product> productShow){
-        Product temp,min;
-        int minIndex;
         for (int i = 0; i < productShow.size(); i++) {
-            temp = productShow.get(i);
-            min = productShow.get(i);
-            minIndex = i;
+            Product temp = productShow.get(i);
+            int minIndex = i;
             for (int j = i+1 ; j < productShow.size() ; j++) {
-                if (min.getPrice()>productShow.get(j).getPrice()){
-                    min = productShow.get(j);
+                if (productShow.get(minIndex).getPrice()>productShow.get(j).getPrice()){
                     minIndex = j;
                 }
             }
-            productShow.set(i,min);
+            productShow.set(i,productShow.get(minIndex));
             productShow.set(minIndex,temp);
         }
     }
 
     public static void sortByPriceDESC(ArrayList<Product> productShow){
-        Product temp,max;
-        int maxIndex;
         for (int i = 0; i < productShow.size(); i++) {
-            temp = productShow.get(i);
-            max = productShow.get(i);
-            maxIndex = i;
+            Product temp = productShow.get(i);
+            int maxIndex = i;
             for (int j = i+1 ; j < productShow.size()  ; j++) {
-                if (max.getPrice()<productShow.get(j).getPrice()){
-                    max = productShow.get(j);
+                if (productShow.get(maxIndex).getPrice()<productShow.get(j).getPrice()){
                     maxIndex = j;
                 }
             }
-            productShow.set(i,max);
+            productShow.set(i,productShow.get(maxIndex));
             productShow.set(maxIndex,temp);
         }
     }
